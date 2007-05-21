c8f62499a39431db6e018d72a7ebe0c31752b259
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-05-21 17:48:13 +0000

    Add Permission.authorized_for.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -17,5 +17,16 @@ class Permission < ActiveRecord::Base
       controller_path, action = Controller.recognize_path(options)
       !Permission.find(:first, :include => :controller, :conditions => ['path = ? AND (action IS NULL OR action = ?)', controller_path, action]).nil?
     end
+    
+    # Finds all roles that are authorized for the given url
+    def authorized_for(options = '')
+      controller_path, action = Controller.recognize_path(options)
+      controller = Controller.new(:path => controller_path)
+      
+      find(:all,
+        :include => :controller,
+        :conditions => ['path IN (?) AND (action IS NULL OR action = ?)', controller.possible_path_matches, action]
+      )
+    end
   end
 end
